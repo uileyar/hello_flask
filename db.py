df72336309899993b54b6bc67539f050c364c4eb
john <john@chinawebeye.com>
2016-10-13 21:07:14 +0800

    2

--- db.py
+++ db.py
@@ -4,7 +4,7 @@
 
 import sqlite3
 from contextlib import closing
-
+from flask import g
 
 # sqlite3 /tmp/flaskr.db < schema.sql
 def connect_db(app):
@@ -17,3 +17,9 @@ def init_db(app, schema_file):
             db.cursor().executescript(f.read())
         db.commit()
 
+
+def query_db(query, args=(), one=False):
+    cur = g.db.execute(query, args)
+    rv = [dict((cur.description[idx][0], value)
+               for idx, value in enumerate(row)) for row in cur.fetchall()]
+    return (rv[0] if rv else None) if one else rv
--- hello_flask.py
+++ hello_flask.py
@@ -7,15 +7,19 @@ import db
 
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
-# configuration
-DATABASE = 'flaskr.db'
-DEBUG = True
-SECRET_KEY = '<Oc\xe90X\xd2\xe3\xce`\xd2\xf4\xdd\xd2\xdc{P\x1dy\xe5\xce\xd1.\xe1'
-USERNAME = 'admin'
-PASSWORD = 'default'
 
 app = Flask(__name__)
-app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    #DATABASE=os.path.join(os.path.dirname(__file__), 'flaskr.db'),
+    DEBUG=True,
+    SECRET_KEY='<Oc\xe90X\xd2\xe3\xce`\xd2\xf4\xdd\xd2\xdc{P\x1dy\xe5\xce\xd1.\xe1',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 @app.before_request
@@ -32,10 +36,49 @@ def get_secret_key():
     return os.urandom(24)
 
 
-if __name__ == '__main__':
-    if not os.path.exists(os.path.join(os.path.dirname(__file__), app.config['DATABASE'])):
-        db.init_db(app, 'schema.sql')
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
 
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
+if __name__ == '__main__':
+    if not os.path.exists(app.config['DATABASE']):
+        db.init_db(app, 'schema.sql')
     app.run()
 
 
